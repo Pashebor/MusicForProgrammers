38a8a93a1a6e3121a0160d3b080f8a9f67cd5ccd
p.demyanov <p.demyanov@x5.ru>
2024-02-25 15:27:33 +0300

    The app icon has been updated

--- app/src/main/java/com/example/musicforprogrammers/activities/StartScreenActivity.kt
+++ app/src/main/java/com/example/musicforprogrammers/activities/StartScreenActivity.kt
@@ -3,7 +3,6 @@ package com.example.musicforprogrammers.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
 import com.example.musicforprogrammers.R
 import com.example.musicforprogrammers.views.MFPTextView
 
