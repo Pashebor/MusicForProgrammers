851072e32e9e859f60986bb75afbe858386dde49
p.demyanov <p.demyanov@x5.ru>
2024-02-26 23:17:35 +0300

    The button play tap sound effect has been added

--- app/src/main/java/com/example/musicforprogrammers/activities/StartScreenActivity.kt
+++ app/src/main/java/com/example/musicforprogrammers/activities/StartScreenActivity.kt
@@ -1,10 +1,12 @@
 package com.example.musicforprogrammers.activities
 
+import android.content.Context
+import android.media.AudioManager
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import com.example.musicforprogrammers.R
-import com.example.musicforprogrammers.views.MFPTextView
 
 class StartScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,10 +14,17 @@ class StartScreenActivity : AppCompatActivity() {
         setContentView(R.layout.start_screen)
 
         val button = findViewById<Button>(R.id.button_touch)
-        val textView = findViewById<MFPTextView>(R.id.hello)
+        val player = MediaPlayer.create(this, R.raw.button_tap_sound)
 
         button.setOnClickListener {
-            textView.text = "Clicked!"
+            val phoneAudio: AudioManager = getSystemService(Context.AUDIO_SERVICE) as AudioManager
+            val currentVolume = phoneAudio.getStreamVolume(AudioManager.STREAM_MUSIC).toFloat()
+            val maxVolume = phoneAudio.getStreamMaxVolume(AudioManager.STREAM_MUSIC).toFloat()
+            val backwardPercentage = (100f - (currentVolume / maxVolume * 100f)) / 100f
+            val beepSoundVolume = if (backwardPercentage < 0.1f) 0.1f else backwardPercentage
+
+            player.setVolume(beepSoundVolume, beepSoundVolume)
+            player.start()
         }
     }
 }
